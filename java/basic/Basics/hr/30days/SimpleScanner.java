1337faaecee822748fd9d041b13a8a766d759939
Lefs Code <el.lampros@gmail.com>
2018-09-06 12:02:29 +0300

    SimpleScanner

---
+++ java/basic/Basics/hr/30days/SimpleScanner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SimpleScanner {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		scanner.close();
+
+		System.out.println(input);
+
+	
+	}
+
+	
+}
+
